9f0beddfede5044dc6c44f1fb8f1d8bbc9bb26e0
JGames101 <jbmadden@hotmail.ca>
2018-04-30 21:33:01 -0700

    FULL pwa support

--- src/javascript.js
+++ src/javascript.js
@@ -3,6 +3,19 @@ drawerState = {
     open:false,
     mouseDown:false
 };
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('sw.js')
+        .then(function(swReg) {
+            console.log('Service Worker is registered', swReg);
+            swRegistration = swReg;
+        })
+        .catch(function(error) {
+            console.error('Service Worker Error', error);
+        });
+            
+    });
+};
 colours = [
     ["#1675d1", "#00796b"],
     ["#fbc02d", "#689f38"],
